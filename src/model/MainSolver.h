7dee17ed2d6120617de042135b766b6193166c74
youyusama <xiaozi465@163.com>
2022-06-14 16:22:00 +0800

    back to mini

--- src/model/MainSolver.h
+++ src/model/MainSolver.h
@@ -1,27 +1,15 @@
 #ifndef MAINSOLVER_H
 #define MAINSOLVER_H
 
-#ifdef CADICAL
-#include "CarSolver_cadical.h"
-#else
 #include "CarSolver.h"
-#endif
 
 namespace car
 {
 
-#ifdef CADICAL
-class MainSolver : public CarSolver_cadical
-{
-public:
-    MainSolver(std::shared_ptr<AigerModel> model, bool isForward);
-#else
 class MainSolver : public CarSolver
 {
 public:
     MainSolver(std::shared_ptr<AigerModel> model, bool isForward);
-#endif
-
 private:
 
 };
